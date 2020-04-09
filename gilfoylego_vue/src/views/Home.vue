a858007f0846f3cab6a4c4f92d1680926ad31dfb
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-09 14:18:45 +0200

    formuläret ute, inte funktioner plus nåt fix kvar

--- gilfoylego_vue/src/views/Home.vue
+++ gilfoylego_vue/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home text-center">
+    <div>
+      <p>Sök på din drömdestination:</p>
+    </div>
+    <div class="container">
+        <form>
+          <div class="row justify-content-center">
+            <div class="column">
+              Hotell, stad eller land:
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="column">
+              <input type="text">
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="column text-left">
+              Från
+            </div>
+            <div class="column">
+              Till
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="column">
+              <input type="date">
+            </div>
+            <div class="column">
+              <input type="date">
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="column">
+              Antal vuxna:
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="column">
+              <input type="number">
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="column">
+              Antal barn:
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="column">
+              <input type="number">
+            </div>
+          </div>
+          <div class="row d-flex justify-content-center">
+            <div class="column p-1">
+              <button>Sök</button>
+            </div>
+          </div>
+        </form>
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
