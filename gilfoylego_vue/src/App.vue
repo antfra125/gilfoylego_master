c3d0cc3ae04cdf616c8b5c7d7099d528db01c76d
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-09 11:25:02 +0200

    en början med bootstrap samt stylesheet

--- gilfoylego_vue/src/App.vue
+++ gilfoylego_vue/src/App.vue
@@ -1,32 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Gilfoyle Go</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="d-flex justify-content-between" id="nav">
+      <span class="title p-2"><router-link to="/">Gilfoyle Go</router-link></span>
+      <span class="user p-2"><router-link to="/about"><img src="./images/user.png"></router-link></span>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style src="./style.css"></style>
+<style src="./bootstrap.min.css"></style>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
