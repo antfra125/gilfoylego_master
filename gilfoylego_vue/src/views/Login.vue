2d6e7b986fde51adb5266f4b3a9bd0ac9ac05720
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-14 19:41:56 +0200

    login skapad, länk till reg kvar!

---
+++ gilfoylego_vue/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex justify-content-center pt-5">
+        <form class="text-center">
+            <div class="form-group">
+                <input type="text" placeholder="Användarnamn/E-mail">
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Lösenord">
+            </div>
+            <div class="form-group">
+                <button>Logga in</button>
+            </div>
+            <div>
+                Inget konto? Skapa ett här, utan kostnad!
+            </div>
+        </form>
+        
+    </div>
+</template>
