b31cf60624eb9575194552e543cd0c4045919af3
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-04-14 14:26:11 +0200

    Update index.js

--- gilfoylego_vue/src/router/index.js
+++ gilfoylego_vue/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -11,20 +10,10 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/HotelDescription',
-    name: 'HotelDescription',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Hoteldescription.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/HotelDescription.vue')
   }
 ]
 
