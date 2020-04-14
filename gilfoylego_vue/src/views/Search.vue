6b777ac093d442176a0c78b83427722fddd58e28
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-14 14:55:24 +0200

    omarbetning av grid

--- gilfoylego_vue/src/views/Search.vue
+++ gilfoylego_vue/src/views/Search.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="d-flex justify-content-center">
-        <form>
-          <div class="row d-flex justify-content-center">
-            <div class="column">
-              <input type="text" size="35" placeholder="Hotell, stad eller land"> 
-              <button class="custom-search-button"><img class="pb-1" src="../images/search.png"></button>
-            </div>
+    <div>
+      <div class="container">
+        <div class="row d-flex justify-content-center">
+          <input type="text" size="35" placeholder="Hotell, stad eller land"> 
+          <button class="custom-search-button"><img class="pb-1" src="../images/search.png"></button>
+        </div>
+        <div class="row d-flex justify-content-between">
+          <div class="column">
+            Från
           </div>
-          <div class="row d-flex justify-content-between">
-            <div class="column text-left">
-              Från
-            </div>
-            <div class="column">
-              Till
-            </div>
+          <div class="column text-right">
+            Till
           </div>
-          <div class="row d-flex justify-content-center">
-            <div class="column">
-              <input type="date" min="2020-07-01" max="2020-08-30">
-            </div>
-            <div class="column">
-              <input type="date" min="2020-07-02" max="2020-08-31">
-            </div>
+        </div>
+        <div class="row d-flex justify-content-between">
+          <div class="column">
+            <input type="date" min="2020-07-01" max="2020-08-30">
           </div>
-          <div class="row d-flex justify-content-center">
-            <div class="column pt-1">
-              <input type="number" size="39" min="1" placeholder="Antal rum">
-            </div>
+          <div class="column">
+            <input type="date" min="2020-07-02" max="2020-08-31">
           </div>
-          <div class="row d-flex justify-content-center">
+        </div>
+        <div class="row d-flex justify-content-center pt-1">
+          <input type="number" size="39" min="1" placeholder="Antal rum">
+        </div>
+        <div class="row d-flex justify-content-center">
             <div class="column p-1">
             </div>
           </div>
@@ -66,6 +63,32 @@
               <input type="number" size="39" placeholder="Maxavstånd till centrum">
             </div>
           </div>
-        </form>
+      </div>
+      <div class="container pt-5">
+        <div class="row result">
+          <div class="col-4 pic-col">
+            <img src="../images/ex.jpg">
+          </div>
+          <div class="col-8">
+            <div class="row">
+              <strong>Hotellets Namn</strong>
+            </div>
+            <div class="row">
+              Omdöme
+            </div>
+            <div class="row">
+              Avstånd till stand
+            </div>
+            <div class="row">
+              Avstånd till centrum
+            </div>
+            <div class="row text-right">
+              Från ?kr
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  
 </template>
