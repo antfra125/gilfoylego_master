da3542ad2ccd009fdd0c679d3723d62139d70ed1
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-04-14 14:06:28 +0200

    Update index.js

--- gilfoylego_vue/src/router/index.js
+++ gilfoylego_vue/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import HotelDescription from '../views/HotelDescription.vue'
 
 Vue.use(VueRouter)
 
@@ -21,7 +20,7 @@ Vue.use(VueRouter)
   },
   {
     path: '/HotelDescription',
-    name: 'Hotel Description',
+    name: 'HotelDescription',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
