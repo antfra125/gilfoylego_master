1881fa77c2782b4fe1ff758fc2d7701de7b39a0c
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-14 21:54:27 +0200

    formdesignfixar

--- gilfoylego_vue/src/views/Home.vue
+++ gilfoylego_vue/src/views/Home.vue
@@ -5,36 +5,32 @@
     </div>
     <div class="d-flex justify-content-center">
         <form>
-          <div class="row d-flex justify-content-center">
-            <div class="column">
-              <input type="text" size="39" placeholder="Hotell, stad eller land"> 
-            </div>
+          <div class="row d-flex justify-content-center form-group">
+              <input type="text" class="form-control" placeholder="Hotell, stad eller land"> 
           </div>
-          <div class="row d-flex justify-content-between">
-            <div class="column text-left">
+          <div class="row form-group d-flex justify-content-between">
+          <div class="col-6">
+            <div class="row">
               Från
             </div>
-            <div class="column">
+            <div class="row">
+              <input type="date" class="form-control" min="2020-07-01" max="2020-08-30">
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="row d-flex justify-content-end">
               Till
             </div>
-          </div>
-          <div class="row d-flex justify-content-center">
-            <div class="column">
-              <input type="date" min="2020-07-01" max="2020-08-30">
-            </div>
-            <div class="column">
-              <input type="date" min="2020-07-02" max="2020-08-31">
+            <div class="row">
+              <input type="date" class="form-control" min="2020-07-02" max="2020-08-31">
             </div>
           </div>
-          <div class="row d-flex justify-content-center">
-            <div class="column pt-1">
-              <input type="number" size="39" min="1" placeholder="Antal rum">
-            </div>
+        </div>
+          <div class="row form-group d-flex justify-content-center">
+              <input type="number" class="form-control" min="1" placeholder="Antal rum">
           </div>
-          <div class="row d-flex justify-content-center">
-            <div class="column p-1">
+          <div class="row form-group d-flex justify-content-center">
               <button>Sök</button>
-            </div>
           </div>
         </form>
     </div>
