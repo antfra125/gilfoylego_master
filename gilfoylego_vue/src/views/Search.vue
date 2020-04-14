4ace8ff947f612d6c1c25120c3364926d499307a
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-14 21:50:50 +0200

    designfixar

--- gilfoylego_vue/src/views/Search.vue
+++ gilfoylego_vue/src/views/Search.vue
@@ -2,34 +2,38 @@
   <div class="d-flex justify-content-center">
     <div>
       <div class="container">
-        <div class="row d-flex justify-content-center">
-          <input type="text" size="35" placeholder="Hotell, stad eller land"> 
-          <button class="custom-search-button"><img class="pb-1" src="../images/search.png"></button>
-        </div>
-        <div class="row d-flex justify-content-between">
-          <div class="column">
-            Från
-          </div>
-          <div class="column text-right">
-            Till
+        <div class="row form-group d-flex justify-content-center input-group mt-auto">
+          <input type="text" class="form-control" placeholder="Hotell, stad eller land">
+          <div class="input-group-append">
+            <button class="form-control custom-search-button"><img class="pb-1" src="../images/search.png"></button>
           </div>
         </div>
-        <div class="row d-flex justify-content-between">
-          <div class="column">
-            <input type="date" min="2020-07-01" max="2020-08-30">
+        <div class="row form-group d-flex justify-content-between">
+          <div class="col-6">
+            <div class="row">
+              Från
+            </div>
+            <div class="row">
+              <input type="date" class="form-control" min="2020-07-01" max="2020-08-30">
+            </div>
           </div>
-          <div class="column">
-            <input type="date" min="2020-07-02" max="2020-08-31">
+          <div class="col-6">
+            <div class="row d-flex justify-content-end">
+              Till
+            </div>
+            <div class="row">
+              <input type="date" class="form-control" min="2020-07-02" max="2020-08-31">
+            </div>
           </div>
         </div>
-        <div class="row d-flex justify-content-center pt-1">
-          <input type="number" size="39" min="1" placeholder="Antal rum">
+        <div class="row form-group d-flex justify-content-center">
+          <input type="number" class="form-control" min="1" placeholder="Antal rum">
         </div>
         <div class="row d-flex justify-content-center">
             <div class="column p-1">
             </div>
           </div>
-          <div class="row sort d-flex justify-content-between">
+          <div class="row form-group sort d-flex justify-content-between">
             <span class="column">Sortera efter:</span>
             <span class="column"><img src="../images/sort.png"> Pris</span>
             <span class="column"><img src="../images/sort.png"> Omdömde</span>
@@ -45,7 +49,7 @@
             <input type="checkbox"> Barnklubb
             </div>
         </div>
-        <div class="row d-flex justify-content-between">
+        <div class="row form-group d-flex justify-content-between">
             <div class="column">
               <input type="checkbox"> Kvällsunderhållning
             </div>
@@ -53,23 +57,19 @@
             <input type="checkbox"> Restaurang
             </div>
         </div>
+        <div class="row form-group">
+          <input type="number" class="form-control" min="0" placeholder="Maxavstånd till strand">
+        </div>
         <div class="row">
-            <div class="column pt-1">
-              <input type="number" size="39" placeholder="Maxavstånd till strand">
-            </div>
-          </div>
-          <div class="row">
-            <div class="column pt-1">
-              <input type="number" size="39" placeholder="Maxavstånd till centrum">
-            </div>
-          </div>
+          <input type="number" class="form-control" min="0" placeholder="Maxavstånd till centrum">
+        </div>
       </div>
       <div class="container pt-5">
         <div class="row result">
           <div class="col-4 pic-col">
             <img src="../images/ex.jpg">
           </div>
-          <div class="col-8">
+          <div class="col-8 pl-2">
             <div class="row">
               <strong>Hotellets Namn</strong>
             </div>
@@ -82,7 +82,7 @@
             <div class="row">
               Avstånd till centrum
             </div>
-            <div class="row text-right">
+            <div class="row d-flex justify-content-end">
               Från ?kr
             </div>
           </div>
