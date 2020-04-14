ab0b9085c4fadffb387810a4a3644f7950015e65
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-04-14 13:54:37 +0200

    Update HotelDescription.vue

--- gilfoylego_vue/src/views/HotelDescription.vue
+++ gilfoylego_vue/src/views/HotelDescription.vue
@@ -1,7 +1,16 @@
 <template>
     <section class="HotelDescription">
-        <div>
-            
+        <div class="d-flex">
+            <img src="('{id}')">
         </div>
+        <div>
+            <p id="thisIsTheDescription">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium hendrerit est, 
+                ac dignissim turpis. Vivamus non scelerisque metus. Aliquam erat volutpat. Aenean 
+                malesuada ex magna, sit amet porta nisl efficitur nec. In neque ligula, pulvinar a 
+                ante ac, viverra aliquet ligula. Praesent ac eros et mauris malesuada interdum. 
+                Praesent in nisl mauris. Phasellus malesuada ac dui in maximus.
+            </p>
     </section>
 </template>
+
