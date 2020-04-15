26698077f35b0d5f3bcb0cb21f2b1348212aabd0
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-04-15 13:59:49 +0200

    Java Code

--- gilfoylego_vue/src/views/HotelDescription.vue
+++ gilfoylego_vue/src/views/HotelDescription.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="HotelDescription">
         <div class="d-flex">
-            <img class="d-flex" src="('{id}')">
+            <img class="d-flex" src=" ../img/hotel1.jpg">
             <p class="d-flex" id="thisIsTheDescription">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium hendrerit est, 
                 ac dignissim turpis. Vivamus non scelerisque metus. Aliquam erat volutpat. Aenean 
@@ -10,5 +10,25 @@
                 Praesent in nisl mauris. Phasellus malesuada ac dui in maximus.
             </p>
         </div>
+        <section>
+            <div class="d-flex">
+                <div class="justify-content-around">
+                    <input type="checkbox" id="addOn" name="addOn" value="Pool">
+                </div>
+                
+                <div class="justify-content-around">
+                    <input type="checkbox" id="addOn" name="addOn" value="KidsClub">
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="justify-content-around">
+                    <input type="checkbox" id="addOn" name="addOn" value="Resturant">
+                </div>
+
+                <div class="justify-content-around">
+                    <input type="checkbox" id="addOn" name="addOn" value="Entertaiment">
+                </div>
+            </div>
+        </section>
     </section>
 </template>
