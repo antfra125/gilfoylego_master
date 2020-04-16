cc23d7c48fd168b85c759942b495b45b6648101c
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-04-16 09:42:30 +0200

    img,descr,checkboxes,distance är tillagt

--- gilfoylego_vue/src/views/HotelDescription.vue
+++ gilfoylego_vue/src/views/HotelDescription.vue
@@ -1,34 +1,46 @@
 <template>
-    <section class="HotelDescription">
-        <div class="d-flex">
-            <img class="d-flex" src=" ../img/hotel1.jpg">
-            <p class="d-flex" id="thisIsTheDescription">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium hendrerit est, 
-                ac dignissim turpis. Vivamus non scelerisque metus. Aliquam erat volutpat. Aenean 
-                malesuada ex magna, sit amet porta nisl efficitur nec. In neque ligula, pulvinar a 
-                ante ac, viverra aliquet ligula. Praesent ac eros et mauris malesuada interdum. 
-                Praesent in nisl mauris. Phasellus malesuada ac dui in maximus.
-            </p>
-        </div>
-        <section>
-            <div class="d-flex">
-                <div class="justify-content-around">
-                    <input type="checkbox" id="addOn" name="addOn" value="Pool">
-                </div>
-                
-                <div class="justify-content-around">
-                    <input type="checkbox" id="addOn" name="addOn" value="KidsClub">
-                </div>
-            </div>
-            <div class="d-flex">
-                <div class="justify-content-around">
-                    <input type="checkbox" id="addOn" name="addOn" value="Resturant">
-                </div>
-
-                <div class="justify-content-around">
-                    <input type="checkbox" id="addOn" name="addOn" value="Entertaiment">
-                </div>
-            </div>
-        </section>
+  <section class="container HotelDescription">
+    <section class="row d-flexmb-1 mt-4">
+      <img class="col-4 d-flex" src="../img/hotel1.jpg" />
+      <p class="col-6 d-flex ml-5" id="thisIsTheDescription">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium hendrerit est,
+        ac dignissim turpis. Vivamus non scelerisque metus. Aliquam erat volutpat. Aenean
+        malesuada ex magna, sit amet porta nisl efficitur nec. In neque ligula, pulvinar a
+        ante ac, viverra aliquet ligula. Praesent ac eros et mauris malesuada interdum.
+        Praesent in nisl mauris. Phasellus malesuada ac dui in maximus.
+      </p>
     </section>
+    <section class="row ml-1 mb-3">
+      <p>2199/night</p>
+    </section>
+
+    <section class="row">
+      <div class="col">
+        <input type="checkbox" id="addOn" name="Pool" value="Pool" />
+        <label for="Pool">Pool</label>
+      </div>
+      <div class="col">
+        <input type="checkbox" id="addOn" name="KidsClub" value="KidsClub" />
+        <label for="KidsClubs">KidsClubs</label>
+      </div>
+    </section>
+
+    <section class="row mb-3">
+      <div class="col">
+        <input type="checkbox" id="addOn" name="Resturant" value="Resturant" />
+        <label for="Resturant">Resturant</label>
+      </div>
+      <div class="col">
+        <input type="checkbox" id="addOn" name="Entertaiment" value="Entertaiment" />
+        <label for="Entertaiment">Entertaiment</label>
+      </div>
+    </section>
+
+    <section class="row">
+      <div>
+        <p>Distance to the beach: {distanceBeach}</p>
+        <p>Distance to centrum: {distanceCentrum}</p>
+      </div>
+    </section>
+  </section>
 </template>
