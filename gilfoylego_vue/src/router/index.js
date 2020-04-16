6de8bb03bd363e3f2bb7cc5bc0938a4567c4a7e4
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-04-16 13:52:03 +0200

    umok

--- gilfoylego_vue/src/router/index.js
+++ gilfoylego_vue/src/router/index.js
@@ -16,7 +16,6 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/HotelDescription.vue')
     path: '/search',
     name: 'Search',
     component: () => import(/* webpackChunkName: "about" */ '../views/Search.vue')
