3d92d3b80b347cd79211a5237c0e8a200afe3fd5
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-17 08:51:17 +0200

    import tillägg

--- gilfoylego_vue/src/main.js
+++ gilfoylego_vue/src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+import { DropdownPlugin, TablePlugin, FormPlugin, FormInputPlugin, FormSelectPlugin,
+FormCheckboxPlugin, FormDatepickerPlugin, FormGroupPlugin, InputGroupPlugin , ButtonPlugin } from 'bootstrap-vue'
+Vue.use(FormPlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(FormDatepickerPlugin)
+Vue.use(FormCheckboxPlugin)
+Vue.use(FormSelectPlugin)
+Vue.use(FormInputPlugin)
+Vue.use(InputGroupPlugin)
+Vue.use(ButtonPlugin)
 Vue.use(DropdownPlugin)
 Vue.use(TablePlugin)
 
