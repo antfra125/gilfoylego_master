ebf9f6b67fb94e6d360c4dc089a53b8b30ed0510
Magnus <isthisthereallife@riseup.net>
2020-04-20 15:42:58 +0200

    RoomType => Roomtype
    Renamed, and added GetMapping.

--- rest_srvr/src/main/java/com/example/demo/rest/RoomTypeController.java
+++ rest_srvr/src/main/java/com/example/demo/rest/RoomTypeController.java
@@ -1,9 +1,34 @@
 package com.example.demo.rest;
 
+import com.example.demo.entity.Roomtype;
+import com.example.demo.repository.RoomtypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/roomType")
+@RequestMapping("/roomtype")
 public class RoomTypeController {
+
+    @Autowired
+    private RoomtypeRepository roomtypeRepository;
+
+    @GetMapping
+    public Iterable<Roomtype> findAll(){
+        return roomtypeRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Optional<Roomtype> findById(@PathVariable long id){
+        return roomtypeRepository.findById(id);
+    }
+
+    /*
+    @GetMapping("/{name}")
+    public Optional<RoomType>findByName(@PathVariable String name){
+        return roomTypeRepository.findByName(name);
+    }*/
 }
