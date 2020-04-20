9af3f95e28eae48159dd466a9b624b02921a4a1e
Magnus <isthisthereallife@riseup.net>
2020-04-20 16:10:52 +0200

    WIP

--- rest_srvr/src/main/java/com/example/demo/repository/HotelRepository.java
+++ rest_srvr/src/main/java/com/example/demo/repository/HotelRepository.java
@@ -1,6 +1,3 @@
-
-
-
 package com.example.demo.repository;
 
 import com.example.demo.entity.Hotel;
@@ -12,14 +9,14 @@ import java.util.Optional;
 
 @Repository
 public interface HotelRepository extends CrudRepository<Hotel, Long> {
+    Iterable<Hotel> findAll();
     Optional<Hotel> findById(long id);
-
     Iterable<Hotel> findAllByName(String name);
     Iterable<Hotel> findAllByCityId(long cityId);
-    Iterable<Hotel> findAllByHasRestaurant(Boolean hasRestaurant);
-    Iterable<Hotel> findAllByHasPool(Boolean hasPool);
-    Iterable<Hotel> findAllByHasKidsClub(Boolean hasKidsClub);
-    Iterable<Hotel> findAllByHasEveningEntertainment(Boolean hasNightEntertainment);
+    Iterable<Hotel> findAllByHasRestaurant(boolean hasRestaurant);
+    Iterable<Hotel> findAllByHasPool(boolean hasPool);
+    Iterable<Hotel> findAllByHasKidsClub(boolean hasKidsClub);
+    Iterable<Hotel> findAllByHasEveningEntertainment(boolean hasNightEntertainment);
     Iterable<Hotel> findAllByMetersToBeach(int metersToBeach);
     Iterable<Hotel> findAllByMetersToCityCenter(int metersToCityCenter);
     Iterable<Hotel> findAllByRating(double rating);
--- rest_srvr/src/main/java/com/example/demo/rest/HotelController.java
+++ rest_srvr/src/main/java/com/example/demo/rest/HotelController.java
@@ -1,10 +1,58 @@
 package com.example.demo.rest;
 
+import com.example.demo.entity.Hotel;
+import com.example.demo.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hotel") // Parent-URL:n för denna klass
 public class HotelController {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @GetMapping
+    public Iterable<Hotel> findAll(){
+        return hotelRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Hotel> findById(long id){
+        return hotelRepository.findById(id);
+    }
+    @GetMapping("/name/{name}")
+    public Iterable<Hotel> findAllByName(String name){
+        return hotelRepository.findAllByName(name);
+    }
+    @GetMapping("/cityId/{cityId}")
+    public Iterable<Hotel> findAllByCityId(long cityId){
+        return hotelRepository.findAllByCityId(cityId);
+    }
+    @GetMapping("/hasRestaurant")
+    public Iterable<Hotel> findAllByHasRestaurant(boolean hasRestaurant){
+        return hotelRepository.findAllByHasRestaurant(hasRestaurant);
+    }
+    @GetMapping("/hasPool")
+    public Iterable<Hotel> findAllByHasPool(boolean hasPool){
+        return hotelRepository.findAllByHasPool(hasPool);
+    }
+    @GetMapping("/hasKidsClub")
+    public Iterable<Hotel> findAllByHasKidsClub(boolean hasKidsClub){
+        return hotelRepository.findAllByHasKidsClub(hasKidsClub);
+    }
+    @GetMapping("/hasEveningEntertainment")
+    public Iterable<Hotel> findAllByHasEveningEntertainment(boolean hasEveningEntertainment){
+        return hotelRepository.findAllByHasEveningEntertainment(hasEveningEntertainment);
+    }
+    @GetMapping("/beachCloserThan/{metersToBeach}")
+    public Iterable<Hotel> findAllByMetersToBeach(int metersToBeach){
+        var h = hotelRepository.findAllByMetersToBeach(metersToBeach);
+        return null;//TODO this
+    }
 
 }
