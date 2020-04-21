c47e046e7d6dacb151327eec9b453b14bff788b0
Omanas98 <55090516+Omanas98@users.noreply.github.com>
2020-04-21 16:08:17 +0200

    Designen för bokningsbekräftelsen

---
+++ gilfoylego_vue/src/views/Bookingconfirmation.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <h3 class="row ml-1 mb-4">Bekräfta bokning</h3>
+    <div class="row">
+      <img class="col-5" src="../images/hotel1.jpg" />
+      <div class="col-7">
+        <h4>Grande Hotel</h4>
+        <ul>
+          <li class="mb-3">Avstånd till stranden:</li>
+          <li class="mb-4">Avstånd till centrum:</li>
+        </ul>
+        <!-- Iterera över alla checkade boxar för hotelet(pool, underhållning osv) -->
+        <div class="col extra ml-3 mb-2">
+          <img class="pr-2" src="../images/v.png" />
+          <label for="Pool">Pool (Insert/fetch data depending on hotel choice)</label>
+        </div>
+      </div>
+    </div>
+
+    <section class="row mt-4">
+      <!--Insert/fetch right choosen bedtype-->
+      <label class="col-4 choice-bedtype">Rumstyp:</label>
+      <!--Insert/fetch right amount of money for the choosen hotel, room, do the math (person + kids + roomtype) -->
+      <label class="col-8 choice-bedtype">kr för hela sällskapet under <!--{fetch and insert choosen nights here}--></label>
+
+    </section>
+
+    <section class="row mt-3">
+      <!--Insert/fetch right amount of individual('s)-->
+      <label class="col-4 choice-amount-persons">Antal personer:</label>
+      <!--Insert/fetch right amount of kids, if it's choosen to start with-->
+      <label class="col-4 choice-amount-kids">Antal barn:</label>
+    </section>
+  </section>
+</template>
--- gilfoylego_vue/src/views/HotelDescription.vue
+++ gilfoylego_vue/src/views/HotelDescription.vue
@@ -6,42 +6,46 @@
         <div class="row">
           <strong>Hotellets namn</strong>
         </div>
-        <div class="row">
-          Omdöme: ?
-        </div>
+        <div class="row">Omdöme: ?</div>
         <div class="row">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium hendrerit est,
-        ac dignissim turpis. Vivamus non scelerisque metus. Aliquam erat volutpat. Aenean
-        malesuada ex magna, sit amet porta nisl efficitur nec. In neque ligula, pulvinar a
-        ante ac, viverra aliquet ligula. Praesent ac eros et mauris malesuada interdum.
-        Praesent in nisl mauris. Phasellus malesuada ac dui in maximus.
+          ac dignissim turpis. Vivamus non scelerisque metus. Aliquam erat volutpat. Aenean
+          malesuada ex magna, sit amet porta nisl efficitur nec. In neque ligula, pulvinar a
+          ante ac, viverra aliquet ligula. Praesent ac eros et mauris malesuada interdum.
+          Praesent in nisl mauris. Phasellus malesuada ac dui in maximus.
         </div>
       </span>
     </section>
     <section class="row ml-1 mb-3">
-    <span v-if="form.roomtype == 'Enkelrum'"><p>799kr/natt</p></span>
-    <span v-if="form.roomtype == 'Dubbelrum'"><p>1299kr/natt</p></span>
-    <span v-if="form.roomtype == 'Familjerum'"><p>1699kr/natt</p></span>
+      <span v-if="form.roomtype == 'Enkelrum'">
+        <p>799kr/natt</p>
+      </span>
+      <span v-if="form.roomtype == 'Dubbelrum'">
+        <p>1299kr/natt</p>
+      </span>
+      <span v-if="form.roomtype == 'Familjerum'">
+        <p>1699kr/natt</p>
+      </span>
     </section>
 
     <section class="row">
       <div class="col extra">
-        <img class="pr-2" src="../images/v.png">
+        <img class="pr-2" src="../images/v.png" />
         <label for="Pool">Pool</label>
       </div>
       <div class="col extra">
-        <img class="pr-2" src="../images/x.png">
+        <img class="pr-2" src="../images/x.png" />
         <label for="KidsClubs">Barnklubb</label>
       </div>
     </section>
 
     <section class="row mb-3">
       <div class="col extra">
-        <img class="pr-2" src="../images/v.png">
+        <img class="pr-2" src="../images/v.png" />
         <label for="Resturant">Resturang</label>
       </div>
       <div class="col extra">
-        <img class="pr-2" src="../images/v.png">
+        <img class="pr-2" src="../images/v.png" />
         <label for="Entertaiment">Underhållning</label>
       </div>
     </section>
@@ -55,82 +59,78 @@
 
     <section class="mb-5">
       <h4 class="mb-4">Rum</h4>
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <div class="row ml-1">
-        <h5 class="mr-3">Rumstyp</h5>
-        <b-form-select
-          id="input-3"
-          v-model="form.roomtype"
-          :options="roomtypes"
-          required
-        ></b-form-select>
-      </div>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <div class="row ml-1">
+          <h5 class="mr-3">Rumstyp</h5>
+          <b-form-select id="input-3" v-model="form.roomtype" :options="roomtypes" required></b-form-select>
+        </div>
 
-      <div class="row mt-3 ml-1">
-        <h5 class="mr-3">Extrasäng</h5>
-              <b-form-checkbox-group v-model="form.extrabed" id="checkboxes-4">
-          <b-form-checkbox value="true"></b-form-checkbox>
-        </b-form-checkbox-group>
-      </div>
+        <div class="row mt-3 ml-1">
+          <h5 class="mr-3">Extrasäng</h5>
+          <b-form-checkbox-group v-model="form.extrabed" id="checkboxes-4">
+            <b-form-checkbox value="true"></b-form-checkbox>
+          </b-form-checkbox-group>
+        </div>
 
-      <div class="row mt-3 ml-1">
-        <h5 class="mr-3">Antal vuxna</h5>
-        <b-form-input
-              type="number"
-              min="1"
-              v-model="form.amountOfGrownUps"
-              required
-              placeholder="Antal vuxna"
-            ></b-form-input>
-      </div>
+        <div class="row mt-3 ml-1">
+          <h5 class="mr-3">Antal vuxna</h5>
+          <b-form-input
+            type="number"
+            min="1"
+            v-model="form.amountOfGrownUps"
+            required
+            placeholder="Antal vuxna"
+          ></b-form-input>
+        </div>
 
-      <div class="row mt-3 ml-1">
-        <h5 class="mr-3">Antal barn</h5>
-        <b-form-input
-              type="number"
-              min="1"
-              v-model="form.amountOfKids"
-              required
-              placeholder="Antal barn"
-            ></b-form-input>
-      </div>
-      <b-button class="mt-3">Boka</b-button>
+        <div class="row mt-3 ml-1">
+          <h5 class="mr-3">Antal barn</h5>
+          <b-form-input
+            type="number"
+            min="1"
+            v-model="form.amountOfKids"
+            required
+            placeholder="Antal barn"
+          ></b-form-input>
+        </div>
+        <router-link to="/bookingconfirmation">
+          <b-button class="mt-3">Boka</b-button>
+        </router-link>
       </b-form>
     </section>
-    
   </section>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        form: {
-          amountOfGrownUps: '',
-          amountOfKids: '',
-          roomtype: 'Enkelrum',
-          extrabed: []
-        },
-        roomtypes: ['Enkelrum', 'Dubbelrum', 'Familjerum'],
-        show: true
-      }
-    },
-    methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
+  data() {
+    return {
+      form: {
+        amountOfGrownUps: "",
+        amountOfKids: "",
+        roomtype: "Enkelrum",
+        extrabed: []
       },
-      onReset(evt) {
-        evt.preventDefault()
-        this.form.amountOfGrownUps = ''
-        this.form.amountOfKids = ''
-        this.form.roomtype = 'Enkelrum'
-        this.form.extrabed = []
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
+      roomtypes: ["Enkelrum", "Dubbelrum", "Familjerum"],
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.form.amountOfGrownUps = "";
+      this.form.amountOfKids = "";
+      this.form.roomtype = "Enkelrum";
+      this.form.extrabed = [];
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     }
   }
+};
 </script>
