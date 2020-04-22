6cd8d49bd05fefa37b03132e712a7c5496d2b9bc
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-04-22 11:59:43 +0200

    börjat lägga till komponenter

---
+++ gilfoylego_vue/src/components/HotelResult.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <div class="container my-4 p-5 grey-border" v-for="hotel in filteredHotels" :key="hotel.id">
+      <router-link to="/hoteldescription">
+        <div class="row result">
+          <div class="col-4 pic-col">
+            <img :src="hotel.imgUrl">
+          </div>
+          <div class="col-8 pl-4">
+            <div class="row">
+              <strong>{{hotel.name}}</strong>
+            </div>
+            <div class="row">
+              Omdöme: {{hotel.rating}}
+            </div>
+            <div class="row">
+              <p v-if="hotel.metersToBeach < 1000">Avstånd till stranden: {{hotel.metersToBeach}} m</p>
+              <p v-if="hotel.metersToBeach > 1000">Avstånd till stranden: {{hotel.metersToBeach / 1000}} km</p>
+            </div>
+            <div class="row">
+              <p v-if="hotel.metersToCityCenter < 1000">Avstånd till centrum: {{hotel.metersToCityCenter}} m</p>
+              <p v-if="hotel.metersToCityCenter > 1000">Avstånd till centrum: {{hotel.metersToCityCenter / 1000}} km</p>
+            </div>
+            <div class="row d-flex justify-content-end">
+              Från ?kr
+            </div>
+          </div>
+        </div>
+      </router-link>
+      </div>
+    </div>
+</template>
