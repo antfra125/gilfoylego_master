2ca57755428fe75791782a9b10bbfbf2d33cac24
T-Lyon <toby_lyon@hotmail.com>
2020-05-08 09:51:29 +0200

    fixes for Selenium

--- gilfoylego_vue/tests/Selenium/step-definitions/search.js
+++ gilfoylego_vue/tests/Selenium/step-definitions/search.js
@@ -52,27 +52,7 @@ this.When(/^and clcik on the search bar and type in <city>$/, async function (ci
     serchField.sendKeys(city);
 });
 
-this.When(/^enter a start date$/, async function () {
 
-    let startDateButton = await this.findElement('#__BVID__66__dialog_');
-    this.click(startDateButton);
-    let startDate = await this.findElement('#__BVID__68__cell - 2020 - 07 -19_ > span');
-    this.click(startDate);
-});
-
-this.When(/^enter a end date$/, async function () {
-
-    let endDateButton = await this.findElement('#__BVID__66__dialog_');
-    this.click(startDateButton);
-    let endDate = await this.findElement('#__BVID__68__cell - 2020 - 07 -22_ > span');
-    this.click(endDate);
-});
-
-this.When(/^hit search$/, async function () {
-
-    let searchButton = await this.findElement('#app > main > div > div.d - flex.justify - content - center > form > div: nth - child(4) > span: nth - child(1) > button');
-    this.click(searchButton);
-});
 
 this.Then(/^my frist search result should be <city>$/, async function () {
 
@@ -90,27 +70,7 @@ this.When(/^and clcik on the search bar and type in <countries>$/, async functio
     serchField.sendKeys(countries);
 });
 
-this.When(/^enter a start date$/, async function () {
 
-    let startDateButton = await this.findElement('#__BVID__66__dialog_');
-    this.click(startDateButton);
-    let startDate = await this.findElement('#__BVID__68__cell - 2020 - 07 -19_ > span');
-    this.click(startDate);
-});
-
-this.When(/^enter a end date$/, async function () {
-
-    let endDateButton = await this.findElement('#__BVID__66__dialog_');
-    this.click(startDateButton);
-    let endDate = await this.findElement('#__BVID__68__cell - 2020 - 07 -22_ > span');
-    this.click(endDate);
-});
-
-this.When(/^hit search$/, async function () {
-
-    let searchButton = await this.findElement('#app > main > div > div.d - flex.justify - content - center > form > div: nth - child(4) > span: nth - child(1) > button');
-    this.click(searchButton);
-});
 
 this.Then(/^my frist search result should be <countries>$/, async function () {
 
@@ -128,27 +88,8 @@ this.When(/^and click on the search bar and type in "Norge"$/, async function ()
     serchField.sendKeys(Norge);
 });
 
-this.When(/^enter a start date$/, async function () {
 
-    let startDateButton = await this.findElement('#__BVID__66__dialog_');
-    this.click(startDateButton);
-    let startDate = await this.findElement('#__BVID__68__cell - 2020 - 07 -19_ > span');
-    this.click(startDate);
-});
 
-this.When(/^enter a end date$/, async function () {
-
-    let endDateButton = await this.findElement('#__BVID__66__dialog_');
-    this.click(startDateButton);
-    let endDate = await this.findElement('#__BVID__68__cell - 2020 - 07 -22_ > span');
-    this.click(endDate);
-});
-
-this.When(/^hit search$/, async function () {
-
-    let searchButton = await this.findElement('#app > main > div > div.d - flex.justify - content - center > form > div: nth - child(4) > span: nth - child(1) > button');
-    this.click(searchButton);
-});
 
 this.Then(/^my hotels results should be between start date and end date$/, async function () {
 
