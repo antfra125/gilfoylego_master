be0e253ce61b9e9d6b9eb1b7901a74fd5ec60d5c
T-Lyon <toby_lyon@hotmail.com>
2020-05-11 10:02:42 +0200

    Fortsatt arbete med Unit-testing

--- gilfoylego_vue/tests/unit/Search.spec.js
+++ gilfoylego_vue/tests/unit/Search.spec.js
@@ -1,4 +1,7 @@
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
 
+import store from '@/store/index.js'
 import { shallowMount } from '@vue/test-utils'
 import Search from '@/views/Search.vue'
 
@@ -22,39 +25,22 @@ describe('Search.vue', () => {
   test('eMax is the correct date', () => {
     expect(component.vm.eMax).toEqual('2020-08-31')
   })
-  test('the form has the required components', () => {
-    expect(component.vm.form).toEqual({
-      "amenities": [], "amountOfRooms": "", "endDate": "",
-      "m2Center": "", "search": "", "startDate": ""
-    })
-  })
   test('page is showing', () => {
     expect(component.vm.show).toEqual(true)
   })
   test('hotels is an array', () => {
     expect(component.vm.hotels).toEqual([])
   })
-  /*test('the list of hotels is retrieved', () => {
-    let results = component.vm.getHotels()
-
-    expect(objectToArray(results)).toEqual(component.vm.hotels)
-  })*/
-  /*test('the search function is working', () => {
-    let results = component.vm.filteredHotels()
-    for (hotel of results) {
-      result = hotel.name.toLowerCase()
-      expect(hotel.name.toLowerCase()).toEqual(result)
-    }
-
-  })*/
-}
-)
-function objectToArray(results) {
-  var result = [];
-  for (var key in results) {
-    if (results.hasOwnProperty(key)) {
-      result.push(results[key]);
-    }
-  }
-  return result;
-}
+  test('it is possible to sort by price', () => {
+    component.vm.$store = store
+    store.state.sortByPrice = sortByPrice
+    component.vm.sortByPrice()
+    expect(sortByprice).toEqual(true)
+  })
+  test('it is possible to sort by rating', () => {
+    component.vm.$store = store
+    store.state.sortByRating = sortByRating
+    component.vm.sortByRating()
+    expect(sortByRating).toEqual(true)
+  })
+})
