e485166897627745fdad0c128e21b6bb9284cee2
T-Lyon <toby_lyon@hotmail.com>
2020-05-12 09:20:35 +0200

    abc

--- gilfoylego_vue/tests/unit/Search.spec.js
+++ gilfoylego_vue/tests/unit/Search.spec.js
@@ -5,42 +5,37 @@ import store from '@/store/index.js'
 import { shallowMount } from '@vue/test-utils'
 import Search from '@/views/Search.vue'
 
+describe('Search-function', () => {
 
-describe('Search.vue', () => {
-
-  const component = shallowMount(Search)
-
-  test('is a Vue component', () => {
-    expect(component.isVueInstance()).toBeTruthy()
+  let mockForm = { 
+    amenities: [],
+    amountOfRooms: "",
+    endDate: "",
+    m2Beach: "",
+    m2Center: "",
+    search: "",
+    startDate: ""
+    }
+  test('fetching the form', async () => {
+  
+    fetch.mockResponseOnce(mockForm)
+    await store.dispatch('getFormdata')
+    expect(store.state.form).toEqual(mockForm)
   })
-  test('sMin is the correct date', () => {
-    expect(component.vm.sMin).toEqual('2020-07-01');
-  })
-  test('sMax is the correct date', () => {
-    expect(component.vm.sMax).toEqual('2020-08-30')
-  })
-  test('eMin is the correct date', () => {
-    expect(component.vm.eMin).toEqual('2020-07-02')
-  })
-  test('eMax is the correct date', () => {
-    expect(component.vm.eMax).toEqual('2020-08-31')
-  })
-  test('page is showing', () => {
-    expect(component.vm.show).toEqual(true)
-  })
-  test('hotels is an array', () => {
-    expect(component.vm.hotels).toEqual([])
-  })
-  test('it is possible to sort by price', () => {
-    component.vm.$store = store
-    store.state.sortByPrice = sortByPrice
-    component.vm.sortByPrice()
-    expect(sortByprice).toEqual(true)
-  })
-  test('it is possible to sort by rating', () => {
-    component.vm.$store = store
-    store.state.sortByRating = sortByRating
-    component.vm.sortByRating()
-    expect(sortByRating).toEqual(true)
+  test('entering search data', async () => {
+    let expectedResult = {
+      amenities: [],
+      amountOfRooms: "",
+      endDate: "",
+      m2Beach: "1000",
+      m2Center: "200",
+      search: "Sverige",
+      startDate: ""
+    }
+    
+    fetch.mockResponseOnce(mockForm)
+    await store.dispatch('getFormdata')
+    store.dispatch('SET_FORM', expectedResult)
+    expect(store.state.form).toEqual(expectedResult)
   })
 })
