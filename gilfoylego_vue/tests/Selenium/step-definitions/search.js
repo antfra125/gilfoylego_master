ca83f6eacdff4a4b1b8de4d915c67981924dfa30
T-Lyon <toby_lyon@hotmail.com>
2020-05-12 13:44:57 +0200

    fixat chromedriver

--- gilfoylego_vue/tests/Selenium/step-definitions/search.js
+++ gilfoylego_vue/tests/Selenium/step-definitions/search.js
@@ -5,9 +5,9 @@ module.exports = function () {
     this.Given(/^that im on the search page$/, async function () {
         await helpers.loadPage('http://localhost:8080/search');
         await driver.findElement(by.css('#__BVID__32'));
-
+    });
 /*---------------------------------------------------------------------------*/
 
-
+}
     
-} 
+
